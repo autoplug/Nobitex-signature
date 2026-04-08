77257476518e460fa63c738032ef97f807450a6f
Alireza <alireza.b83@gmail.com>
2026-04-09 01:53:23 +0330

    Update main.py

--- main.py
+++ main.py
@@ -26,12 +26,20 @@ payload = {
     }
 body = json.dumps(payload)
 body = json.dumps(payload, separators=(',', ':'))
-
 message = (timestamp + method + path + body).encode("utf-8")
 
+
+# چاپ دیباگ
+print("=== DEBUG INFO ===")
+print("BODY:", body)
+print("MESSAGE FOR SIGNATURE:", message.decode())
+print("BODY LENGTH:", len(body))
+print("MESSAGE LENGTH:", len(message))
+
 signature_bytes = private_key.sign(message)
-signature = base64.b64encode(signature_bytes).decode("utf-8")
+#signature = base64.b64encode(signature_bytes).decode("utf-8")
 signature = base64.urlsafe_b64encode(signature_bytes).decode().rstrip("=")
+print("SIGNATURE:", signature)
 
 headers = {
     "Nobitex-Key": PUBLIC_KEY,
